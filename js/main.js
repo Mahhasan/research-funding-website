7ca18f941cc47db371dce9d4b04fc3380c81f3ed
Mahmud Hasan <mhr.swe.522@gmail.com>
2023-08-10 10:46:57 +0600

    updated

--- js/main.js
+++ js/main.js
@@ -420,12 +420,12 @@
   
       });
       $(document).ready(function() {
-      var e = '<div class="prt_floting_customsett">'+
-                  '<a href="" class="tmtheme_fbar_icons"><i class="fa fa-headphones"></i><span>Support</span></a>'+
-                  '<a href="" class="tmtheme_fbar_icons"><i class="themifyicon themifyicon ti-pencil"></i><span>Customization</span></a>'+
-                  '<a href="" class="tmtheme_fbar_icons"><i class="themifyicon ti-shopping-cart"></i><span class="buy_link">Buy<span></span></span></a>'+
-                  '<div class="clearfix"></div>'+
-              '</div>';
+    //   var e = '<div class="prt_floting_customsett">'+
+    //               '<a href="" class="tmtheme_fbar_icons"><i class="fa fa-headphones"></i><span>Support</span></a>'+
+    //               '<a href="" class="tmtheme_fbar_icons"><i class="themifyicon themifyicon ti-pencil"></i><span>Customization</span></a>'+
+    //               '<a href="" class="tmtheme_fbar_icons"><i class="themifyicon ti-shopping-cart"></i><span class="buy_link">Buy<span></span></span></a>'+
+    //               '<div class="clearfix"></div>'+
+    //           '</div>';
   
       $('body').append(e);
   }); 
